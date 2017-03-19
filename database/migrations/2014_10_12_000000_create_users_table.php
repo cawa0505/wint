a52bd6477c39d212d9048cb019a57389d9438996
Sky <mydr@vip.qq.com>
2017-03-19 12:00:35 +0800

    修复数据表顺序问题

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,8 +14,11 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('tel',11)->comment('手机号');
             $table->string('email')->unique();
             $table->string('password');
+            $table->tinyInteger('status')->comment('用户状态');
+            $table->string('ip')->comment('上次登录ip');
             $table->rememberToken();
             $table->timestamps();
         });
