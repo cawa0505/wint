45c69ac05b31ca58857bf72f200b28eb7d87b214
Sky <mydr@vip.qq.com>
2017-06-09 08:40:04 +0800

    admin

--- app/Models/EduUniversityInfo.php
+++ app/Models/EduUniversityInfo.php
@@ -11,6 +11,6 @@ namespace App\Models;
 class EduUniversityInfo extends EduModel
 {
     public function university(){
-        return $this->hasOne('App\Models\ListUniversity','university_id','id');
+        return $this->hasOne('App\Models\ListUniversity','id','university_id');
     }
 }
